a34100b47d129028e4afaf111b09ed3c3368f224
khwx <khwx@github.com>
2026-07-18 13:13:21 +0100

    fix: remove React import from QuestionFeedback.tsx

--- src/components/QuestionFeedback.tsx
+++ src/components/QuestionFeedback.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { generateHint, generateFeedback, generateExplanation } from '@/lib/feedback-system';
 
 interface QuestionFeedbackProps {
